78b8e1b30ac769f7cb932e651747be80ee2bfdef
heeseok <heeseokid@g.shingu.ac.kr>
2025-07-17 19:48:11 +0900

    Project Setting

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-if (!globalThis.crypto) {
-  const crypto = require('crypto');
-  globalThis.crypto = crypto.webcrypto;
-}
-
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
