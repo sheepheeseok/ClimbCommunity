4076052fb396fc50694d9d6c767ee26aec79d554
heeseok <heeseokid@g.shingu.ac.kr>
2025-07-30 22:36:50 +0900

    Navbar 반응형 구현

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,13 +3,14 @@ import { useState } from 'react'
 import './App.css'
 import Home from './pages/home_tap/Home'
 import Header from "@/components/Header";
+import Navbar from "@/components/Navbar";
 
 function App() {
 
   return (
     <>
       <BrowserRouter>
-          <Header />
+          <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />         {/* 첫 페이지 */}
         </Routes>
