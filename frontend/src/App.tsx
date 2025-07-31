a5609c717ce1a9898d27b4c9cc9f717fbae50bb4
joo-hyejin <robori2357@gmail.com>
2025-08-01 04:52:07 +0900

    메인 수정사항 병합 완료료

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,19 +5,21 @@ import Home from './pages/home_tap/Home'
 import Header from "@/components/Header";
 import Navbar from "@/components/Navbar";
 import SignUp from './pages/Sign_up';
-import Login from './pages/login';
 import ResetPassword from './pages/ResetPassword';
+import Login from './pages/Login';
 
 function App() {
 
   return (
     <>
       <BrowserRouter>
+        <Header />
+        <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />         {/* 첫 페이지 */}
           <Route path="/signup" element={<SignUp />} /> {/* 회원가입 페이지 */}
-            <Route path="/login" element={<Login />} /> {/* 로그인 페이지 */}
-            <Route path="/reset-pw" element={<ResetPassword />} /> {/* 로그인 페이지 */}
+          <Route path="/login" element={<Login />} /> {/* 로그인 페이지 */}
+          <Route path="/reset-pw" element={<ResetPassword />} /> {/* 로그인 페이지 */}
         </Routes>
       </BrowserRouter>
     </>
