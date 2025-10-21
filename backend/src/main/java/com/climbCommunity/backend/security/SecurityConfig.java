de016e0b759db95e09c431f6546de1f8a0276b35
yangheeseok <heeseokid@g.shingu.ac.kr>
2025-10-21 22:37:49 +0900

    업데이트

--- backend/src/main/java/com/climbCommunity/backend/security/SecurityConfig.java
+++ backend/src/main/java/com/climbCommunity/backend/security/SecurityConfig.java
@@ -45,7 +45,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOriginPatterns(List.of("http://localhost:*", "http://127.0.0.1:*" , "http://15.152.47.4:*"));
+        config.setAllowedOriginPatterns(List.of("http://localhost:*", "http://127.0.0.1:*" , "http://56.155.30.181", "http://56.155.30.181:*"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
